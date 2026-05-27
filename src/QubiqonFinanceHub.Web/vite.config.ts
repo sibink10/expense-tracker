0b86a61744ce7ed3e788d1768f7acd63f0a407d7
Sibin K <Sibin.k@qubiqon.io>
2026-05-27 21:58:17 +0530

    added frontend deployment settings

--- src/QubiqonFinanceHub.Web/vite.config.ts
+++ src/QubiqonFinanceHub.Web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  envDir: 'env',
   plugins: [react()],
   server: { port: 3000, proxy: { '/api': { target: 'https://localhost:7201', changeOrigin: true, secure: false } } },
   build: { outDir: 'dist', sourcemap: true },
